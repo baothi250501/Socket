7a0aadb8b554b9e4da9084c9cd029a19127c5e7e
baothi250501 <66853454+baothi250501@users.noreply.github.com>
2021-05-23 12:42:34 +0700

    Delete sendJson.py

--- sendJson.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def sendJson(sock, data):
-    jsonData = json.dumps(data).encode()
-    jsonHeader = {"size" : len(jsonData)}
-    sock.sendall(json.dumps(jsonHeader).encode())
-    sock.sendall(jsonData)
